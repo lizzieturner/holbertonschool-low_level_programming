f85555e46d2cc534fca861cf3a6ce84a24271be3
lizzieturner <hello@lizzieturner.com>
2018-01-17 22:31:03 +0000

    Document main function in 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - entry point for c, containing other functions within
+ * Return: 0 if program successfully completes
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
