4c081b722f2e8f60082f60f68b35b7860d712a5e
lizzieturner <hello@lizzieturner.com>
2018-01-18 22:25:17 +0000

    Create 5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main function in c
+ * Return: exit code (0 if no errors)
+ */
+
+int main(void)
+{
+        int n;
+
+        n = '0';
+
+        while (n <= '9')
+        {
+                putchar(n);
+                n++;
+        }
+
+        putchar('\n');
+
+        return (0);
+}
