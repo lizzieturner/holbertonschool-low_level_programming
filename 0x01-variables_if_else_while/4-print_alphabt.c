a7d5f4d0a065b7888ff7ba0323591b22d84769d4
lizzieturner <hello@lizzieturner.com>
2018-01-18 21:12:06 +0000

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - main function in c
+ * Return: exit code (0 if no errors)
+ */
+
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+			ch++;
+		}
+
+		else
+		{
+			ch++;
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
