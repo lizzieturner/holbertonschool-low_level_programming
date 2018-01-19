15553569c9e64cca2e27721eb4825e313d870e5b
lizzieturner <hello@lizzieturner.com>
2018-01-19 03:04:51 +0000

    Trim function to 40 lines in 10-print_comb2.c

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -9,45 +9,34 @@ int main(void)
 {
 	int a;
 	int b;
-	int comma;
-	int space;
-	int end;
 
 	a = 48;
 	b = 48;
-	comma = 44;
-	space = 32;
-	end = 10;
 
 	while (a <= 57 && b <= 57)
 	{
 		putchar(a);
 		putchar(b);
-
 		if (a < 57 || b < 57)
 		{
-			putchar(comma);
-			putchar(space);
-
+			putchar(',');
+			putchar(' ');
 			if (b < 57)
 			{
 				b++;
 			}
-
 			else
 			{
 				a++;
 				b = 48;
 			}
 		}
-
 		else
 		{
 			a++;
 			b++;
-			putchar(end);
+			putchar('\n');
 		}
 	}
-
 	return (0);
 }
