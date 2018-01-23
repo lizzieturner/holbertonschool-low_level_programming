1014be2fb001bd4575c956b19c8ca461e0495de2
lizzieturner <hello@lizzieturner.com>
2018-01-23 07:26:39 +0000

    Create 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 if succesful
+ */
+
+int main(void)
+{
+	int a = 1;
+	int b = 1;
+	int sum_a = 0;
+	int sum_b = 0;
+
+	while (a * 3 < 1024)
+	{
+		sum_a += a * 3;
+		a++;
+	}
+	while (b * 5 < 1024)
+	{
+		sum_b += b * 5;
+		b++;
+	}
+
+	printf("%d\n", sum_a + sum_b);
+
+	return (0);
+}
