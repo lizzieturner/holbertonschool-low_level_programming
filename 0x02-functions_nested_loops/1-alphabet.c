abe41303786bc2d828f0c96e136af202eb23eb16
lizzieturner <hello@lizzieturner.com>
2018-01-23 01:34:10 +0000

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ *Return: 0 if succesful
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
