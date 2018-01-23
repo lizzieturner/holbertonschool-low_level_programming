ad5a96fee28a11b8634e1452cd98e98ffc90b1ab
lizzieturner <hello@lizzieturner.com>
2018-01-23 20:36:17 +0000

    Change initial conditional in 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,47 +9,38 @@ void print_times_table(int n)
 {
 	int a = 0;
 
-	if (n > 0 && n < 16)
+	if (n >= 0 && n <= 15)
 	{
 		while (a <= n)
 		{
-			int b = 0;
+			int b = 1;
 
+			_putchar('0');
 			while (b <= n)
 			{
 				int p = a * b;
 
-				if (b == 0)
+				_putchar(',');
+				_putchar(' ');
+				if (p < 10)
 				{
+					_putchar(' ');
+					_putchar(' ');
 					_putchar(p + '0');
-					b++;
 				}
-
-
+				else if (p > 99)
+				{
+					_putchar((p / 100) + '0');
+					_putchar(((p / 10) % 10) + '0');
+					_putchar((p % 10) + '0');
+				}
 				else
 				{
-					_putchar(',');
 					_putchar(' ');
-					if (p < 10)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(p + '0');
-					}
-					else if (p > 99)
-					{
-						_putchar((p / 100) + '0');
-						_putchar(((p / 10) % 10) + '0');
-						_putchar((p % 10) + '0');
-					}
-					else
-					{
-						_putchar(' ');
-						_putchar((p / 10) + '0');
-						_putchar((p % 10) + '0');
-					}
-					b++;
+					_putchar((p / 10) + '0');
+					_putchar((p % 10) + '0');
 				}
+				b++;
 			}
 			_putchar('\n');
 			a++;
