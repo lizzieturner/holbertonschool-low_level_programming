2581cf0ddec7da198472e009b6d8bf2a62ed44b1
lizzieturner <hello@lizzieturner.com>
2018-01-24 04:00:17 +0000

    Split number into two components in 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
-#include <float.h>
+
+/**
+ * print_big_num - prints two number components as one number;
+ * if the first part is zero, it will not print
+ * @big: first number component (digits 1-10)
+ * @little: second number component (digits 11-end)
+ */
+
+void print_big_num(unsigned long big, unsigned long little)
+{
+	if (big == 0)
+	{
+		printf("%lu, ", little);
+	}
+	else
+	{
+	printf("%lu%lu, ", big, little);
+	}
+}
 
 /**
  * main - entry point in c
@@ -9,32 +27,43 @@
 
 int main(void)
 {
-	long double a = 1;
-	long double b = 2;
+	unsigned long a_big = 0;
+	unsigned long a_little = 1;
+	unsigned long b_big = 0;
+	unsigned long b_little = 2;
+	unsigned long fib_little = 0;
+	unsigned long fib_big = 0;
+	int carry = 0;
 	int n = 3;
 
-	printf("%0.21Le, ", a);
-	printf("%0.21Le, ", b);
+	print_big_num(a_big, a_little);
+	print_big_num(b_big, b_little);
 
 	while (n < 98)
 	{
-		long double fib = a + b;
+		fib_little = a_little + b_little;
+		fib_big = a_big + b_big;
+		carry = fib_little / 10000000000;
 
-		printf("%0.21Le, ", fib);
-		a = b;
-		b = fib;
+		fib_little = fib_little % 10000000000;
+		fib_big = fib_big + carry;
+
+		print_big_num(fib_big, fib_little);
+
+		a_big = b_big;
+		b_big = fib_big;
+		a_little = b_little;
+		b_little = fib_little;
 		n++;
 	}
 
-	while (n == 98)
-	{
-		long double fib = a + b;
+	fib_little = a_little + b_little;
+	fib_big = a_big + b_big;
+	carry = fib_little / 10000000000;
 
-		printf("%0.21Le\n", fib);
-		a = b;
-		b = fib;
-		n++;
-	}
+	fib_little = fib_little % 10000000000;
+	fib_big = fib_big + carry;
+	printf("%lu%lu\n", fib_big, fib_little);
 
 	return (0);
 }
