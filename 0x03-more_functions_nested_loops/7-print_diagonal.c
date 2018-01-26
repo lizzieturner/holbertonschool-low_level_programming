10f2fc74df95eaec71f74b0ac0a6dcde22d89cbc
lizzieturner <hello@lizzieturner.com>
2018-01-26 01:16:16 +0000

    Create 7-print_diagonal.c

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - draw a straight line in the terminal
+ * @n: length of line
+ */
+
+void print_diagonal(int n)
+{
+	int x;
+	int y;
+
+
+	if (n == 0)
+	{
+		_putchar('\n');
+	}
+
+	for (x = 1; x <= n; x++)
+	{
+		for (y = 0; y <= x; y++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
