8f37339f80d0660f127874ceb96c0f9b140ace0c
lizzieturner <hello@lizzieturner.com>
2018-01-26 01:58:29 +0000

    Remove errant space from 8-print_square.c

--- 0x03-more_functions_nested_loops/8-print_square.c
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -10,10 +10,10 @@ void print_square(int size)
 	int line;
 	int hash;
 
-	if (size == 0)
+	if (size <= 0)
 		_putchar('\n');
 
-	for (line = 0; line < size; line ++)
+	for (line = 0; line < size; line++)
 	{
 		for (hash = 0; hash < size; hash++)
 		{
