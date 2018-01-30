717c57e69db3ca70a8e2771ca96603c52e976328
lizzieturner <hello@lizzieturner.com>
2018-01-30 21:16:30 +0000

    Create 7-puts_half.c

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: string
+ */
+
+void puts_half(char *str)
+{
+
+	int length = 0;
+	int start_half = 0;
+
+	while (*(str + length) != '\0')
+	{
+		length++;
+	}
+
+	if (length % 2 == 0)
+		start_half = (length / 2);
+
+	else
+		start_half = ((length - 1) / 2);
+
+	while (start_half <= length)
+	{
+		_putchar(*(str + start_half));
+		start_half++;
+	}
+
+	_putchar('\n');
+}
