8816f8996f33ebc7137708475cdde0ed5fce029e
lizzieturner <hello@lizzieturner.com>
2018-01-30 21:53:23 +0000

    Update if/else statements in 8-print_array.c

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -12,8 +12,8 @@ void print_array(int *a, int n)
 
 	for (x = 0; x < n; x++)
 	{
-		if (x != n - 1)
-			printf("%d, ", a[x]);
+		if (x < n - 1)
+	       		printf("%d, ", a[x]);
 		else
 			printf("%d\n", a[x]);
 	}
