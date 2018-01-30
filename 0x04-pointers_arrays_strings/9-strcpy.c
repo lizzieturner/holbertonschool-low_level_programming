71c96639c2c589f5df7f68babdf3f1a412d590be
lizzieturner <hello@lizzieturner.com>
2018-01-30 22:23:28 +0000

    Add return doc to 9-strcpy.c

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -4,6 +4,8 @@
  * *_strcpy- copies a string to a new buffer
  * @dest: destination for string
  * @src: source string
+ *
+ * Return: the pointer to dest
  */
 
 char *_strcpy(char *dest, char *src)
