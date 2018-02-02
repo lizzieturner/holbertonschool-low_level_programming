74400657b6d79d3352c18f4ce61d06d4d6fc4fb4
lizzieturner <hello@lizzieturner.com>
2018-02-02 07:28:34 +0000

    Account for INT_MIN in 101-print_number.c

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -7,43 +7,28 @@
 
 void print_number(int n)
 {
-	int dig_1, dig_2, dig_3, dig_4, dig_5, dig_6, dig_7, dig_8, dig_9, last;
-	int sum_1, sum_2, sum_3, sum_4, sum_5, sum_6, sum_7, sum_8, sum_9;
+	int digit, sum, last;
+	int divis = 1000000000;
+	unsigned int num;
+
+	num = n;
+	sum = 0;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		num = n * -1;
 	}
 
-	dig_1 = (n / 1000000000) % 10; sum_1 = dig_1;
-	dig_2 = (n / 100000000) % 10; sum_2 = sum_1 + dig_2;
-	dig_3 = (n / 10000000) % 10; sum_3 = sum_2 + dig_3;
-	dig_4 = (n / 1000000) % 10; sum_4 = sum_3 + dig_4;
-	dig_5 = (n / 100000) % 10; sum_5 = sum_4 + dig_5;
-	dig_6 = (n / 10000) % 10; sum_6 = sum_5 + dig_6;
-	dig_7 = (n / 1000) % 10; sum_7 = sum_6 + dig_7;
-	dig_8 = (n / 100) % 10; sum_8 = sum_7 + dig_8;
-	dig_9 = (n / 10) % 10; sum_9 = sum_8 + dig_9;
-	last = n % 10;
+	while (divis > 1)
+	{
+		digit = (num / divis) % 10;
+		sum += digit;
+		if (sum != 0)
+			_putchar(digit + '0');
+		divis = divis / 10;
+	}
 
-	if (sum_1 != 0)
-		_putchar(dig_1 + '0');
-	if (sum_2 != 0)
-		_putchar(dig_2 + '0');
-	if (sum_3 != 0)
-		_putchar(dig_3 + '0');
-	if (sum_4 != 0)
-		_putchar(dig_4 + '0');
-	if (sum_5 != 0)
-		_putchar(dig_5 + '0');
-	if (sum_6 != 0)
-		_putchar(dig_6 + '0');
-	if (sum_7 != 0)
-		_putchar(dig_7 + '0');
-	if (sum_8 != 0)
-		_putchar(dig_8 + '0');
-	if (sum_9 != 0)
-		_putchar(dig_9 + '0');
+	last = num % 10;
 	_putchar(last + '0');
 }
