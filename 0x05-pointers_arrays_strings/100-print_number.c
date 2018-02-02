df9f77d01b2a225dc99eb7d6662827b1e82ad762
lizzieturner <hello@lizzieturner.com>
2018-02-02 06:20:13 +0000

    Account for zeros in middle of number in 100-print_numbers.c

--- 0x05-pointers_arrays_strings/100-print_number.c
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -1,16 +1,14 @@
 #include "holberton.h"
 
 /**
- * function -
- * @:
- *
- * Return:
+ * print_number - print an integer
+ * @n: integer to print
  */
 
 void print_number(int n)
 {
-	int digit_1, digit_2, digit_3, digit_4, digit_5;
-	int digit_6, digit_7, digit_8, digit_9, digit_10;
+	int dig_1, dig_2, dig_3, dig_4, dig_5, dig_6, dig_7, dig_8, dig_9, last;
+	int sum_1, sum_2, sum_3, sum_4, sum_5, sum_6, sum_7, sum_8, sum_9;
 
 	if (n < 0)
 	{
@@ -18,36 +16,35 @@ void print_number(int n)
 		n = -n;
 	}
 
-	digit_1 = (n / 1000000000) % 10;
-	digit_2 = (n / 100000000) % 10;
-	digit_3 = (n / 10000000) % 10;
-	digit_4 = (n / 1000000) % 10;
-	digit_5 = (n / 100000) % 10;
-	digit_6 = (n / 10000) % 10;
-	digit_7 = (n / 1000) % 10;
-	digit_8 = (n / 100) % 10;
-	digit_9 = (n / 10) % 10;
-	digit_10 = n % 10;
+	dig_1 = (n / 1000000000) % 10; sum_1 = dig_1;
+	dig_2 = (n / 100000000) % 10; sum_2 = sum_1 + dig_2;
+	dig_3 = (n / 10000000) % 10; sum_3 = sum_2 + dig_3;
+	dig_4 = (n / 1000000) % 10; sum_4 = sum_3 + dig_4;
+	dig_5 = (n / 100000) % 10; sum_5 = sum_4 + dig_5;
+	dig_6 = (n / 10000) % 10; sum_6 = sum_5 + dig_6;
+	dig_7 = (n / 1000) % 10; sum_7 = sum_6 + dig_7;
+	dig_8 = (n / 100) % 10; sum_8 = sum_7 + dig_8;
+	dig_9 = (n / 10) % 10; sum_9 = sum_8 + dig_9;
+	last = n % 10;
 
 
-	if (digit_1 != 0)
-		_putchar(digit_1 + '0');
-	if (digit_2 != 0)
-                _putchar(digit_2 + '0');
-	if (digit_3 != 0)
-                _putchar(digit_3 + '0');
-	if (digit_4 != 0)
-                _putchar(digit_4 + '0');
-	if (digit_5 != 0)
-                _putchar(digit_5 + '0');
-	if (digit_6 != 0)
-                _putchar(digit_6 + '0');
-	if (digit_7 != 0)
-                _putchar(digit_7 + '0');
-	if (digit_8 != 0)
-                _putchar(digit_8 + '0');
-	if (digit_9 != 0)
-                _putchar(digit_9 + '0');
-
-	_putchar(digit_10 + '0');
+	if (sum_1 != 0)
+		_putchar(dig_1 + '0');
+	if (sum_2 != 0)
+		_putchar(dig_2 + '0');
+	if (sum_3 != 0)
+		_putchar(dig_3 + '0');
+	if (sum_4 != 0)
+		_putchar(dig_4 + '0');
+	if (sum_5 != 0)
+		_putchar(dig_5 + '0');
+	if (sum_6 != 0)
+		_putchar(dig_6 + '0');
+	if (sum_7 != 0)
+		_putchar(dig_7 + '0');
+	if (sum_8 != 0)
+		_putchar(dig_8 + '0');
+	if (sum_9 != 0)
+		_putchar(dig_9 + '0');
+	_putchar(last + '0');
 }
