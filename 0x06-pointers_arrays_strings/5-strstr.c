340ac8b597f86c21c7b602d3311b4605387304ad
lizzieturner <hello@lizzieturner.com>
2018-02-06 06:41:19 +0000

    Create 5-strstr.c

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * _strstr - finds the first occurence of a substring
+ * @haystack: string
+ * @needle: substring
+ *
+ * Return:  pointer to location in string that substring begins
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *start, *p, *t;
+
+	start = haystack;
+
+	while (*start != '0')
+	{
+		p = start;
+		t = needle;
+
+		while (*t != '\0' && *p != '\0' && *t == *p)
+		{
+			t++;
+			p++;
+		}
+
+		if (*t == '\0')
+			return (start);
+
+		start++;
+	}
+
+	return (0);
+}
