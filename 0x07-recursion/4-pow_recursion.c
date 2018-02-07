9d74472e9276e2287c9b590feb603e3c2173db92
lizzieturner <hello@lizzieturner.com>
2018-02-07 21:16:29 +0000

    Create 4-pow_recursion.c

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - returns x to the power of y
+ * @x: number
+ * @y: exponent
+ *
+ * Return: -1 if y is negative, value of x to the power of y otherwise
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	if (y == 1)
+		return (x);
+
+	return (x * _pow_recursion(x, y - 1));
+}
