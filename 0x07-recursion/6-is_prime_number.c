fd7a2c44912bb56ef7bf224b01b1b0087aaf2936
lizzieturner <hello@lizzieturner.com>
2018-02-08 00:35:10 +0000

    Fix betty errors in 6-is_prime_number.c

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -1,22 +1,30 @@
 #include "holberton.h"
 
 /**
- * function -
- * @:
+ * _checkprime - checks for prime numbers by dividing by possible factors
+ * @n: number
+ * @x: possible factor
  *
- * Return:
+ * Return: 1 if prime, 0 otherwise
  */
 
 int _checkprime(int n, int x)
 {
-        if (x == 1)
-                return (1);
+	if (x == 1)
+		return (1);
 
 	if (n % x == 0)
 		return (0);
 
 	return	(_checkprime(n, x - 1));
 }
+
+/**
+ * is_prime_number - identifies prime numbers
+ * @n: prime number
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
 
 
 int is_prime_number(int n)
