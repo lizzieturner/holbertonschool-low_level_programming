1eb4b8e068d21b858cfbc0917924139f5c76746e
lizzieturner <hello@lizzieturner.com>
2018-02-08 04:30:36 +0000

    Change conditional in 100-wildcmp.c

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -22,10 +22,10 @@ int wildcmp(char *s1, char *s2)
 		if (*s1 == '\0')
 			return (0);
 
-		if (*(s1 + 1) != *(s2 + 1))
+		if (*s1 != *(s2 + 1))
 			return (wildcmp(s1 + 1, s2));
 
-		return (wildcmp(s1 + 1, s2 + 1));
+		return (wildcmp(s1, s2 + 1));
 	}
 
 	if (*s1 != *s2)
