2c6ca89c3c5ed6ee4470612086b3b33b7cec4a43
lizzieturner <hello@lizzieturner.com>
2018-02-09 22:48:21 +0000

    Create 100-change.c

---
+++ 0x09-argc_argv/100-change.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point function
+ * @argc: number of arguments
+ * @argv: array containing arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int cash;
+ 	int count = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cash = atoi(argv[1]);
+
+	if (cash < 0)
+		printf("0\n");
+
+	if (cash > 0)
+	{
+		while (cash >= 25)
+		{
+			cash -= 25;
+			count++;
+		}
+
+		while (cash >= 10)
+		{
+			cash -= 10;
+			count++;
+		}
+
+		while (cash >= 5)
+		{
+			cash -= 5;
+			count++;
+		}
+
+		while (cash >= 1)
+		{
+			cash -= 1;
+			count++;
+		}
+	}
+
+	printf("%d\n", count);
+	return (0);
+}
