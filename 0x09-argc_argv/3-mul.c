c72607190e1e10f388e6ccfe1bb81bcb10806a65
lizzieturner <hello@lizzieturner.com>
2018-02-09 21:27:01 +0000

    Create 3-mul.c

---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point function
+ * @argc: number of arguments
+ * @argv: array containing arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int product;
+
+	if (argc != 3)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+
+	product = atoi(argv[1]) * atoi(argv[2]);
+
+	printf("%d\n", product);
+
+	return (0);
+}
