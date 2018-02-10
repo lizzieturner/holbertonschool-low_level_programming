3e662d7ea8a0864c1650e9fadc780d7c6062c484
lizzieturner <hello@lizzieturner.com>
2018-02-10 21:06:11 +0000

    Account for 2 cent coin in 100-change.c

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -11,8 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-	int cash;
- 	int count = 0;
+	int cash, coins;
 
 	if (argc != 2)
 	{
@@ -22,36 +21,30 @@ int main(int argc, char *argv[])
 
 	cash = atoi(argv[1]);
 
-	if (cash < 0)
-		printf("0\n");
-
-	while (cash > 0)
+	if (cash <= 0)
 	{
-		while (cash >= 25)
-		{
-			cash -= 25;
-			count++;
-		}
-
-		while (cash >= 10)
-		{
-			cash -= 10;
-			count++;
-		}
-
-		while (cash >= 5)
-		{
-			cash -= 5;
-			count++;
-		}
-
-		while (cash >= 1)
-		{
-			cash -= 1;
-			count++;
-		}
+		printf("0\n");
+		return (0);
 	}
 
-	printf("%d\n", count);
+	for (coins = 0; cash > 0; coins++)
+	{
+		if (cash >= 25)
+			cash -= 25;
+
+		else if (cash >= 10)
+			cash -= 10;
+
+		else if (cash >= 5)
+			cash -= 5;
+
+		else if (cash >= 2)
+			cash -= 2;
+
+		else
+			cash -= 1;
+	}
+
+	printf("%d\n", coins);
 	return (0);
 }
