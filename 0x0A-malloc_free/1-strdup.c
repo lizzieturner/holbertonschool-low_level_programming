dcdd7232202824b4526acfb9fd8ed56a13264155
lizzieturner <hello@lizzieturner.com>
2018-02-14 06:19:05 +0000

    Create 1-strdup.c

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - allocate a space in memory containing a copy of a string
+ * @str: string to copy
+ *
+ * Return: pointer to new string
+ * or NULL is str = NULL or insufficient memory available
+ */
+
+char *_strdup(char *str)
+{
+	char *new;
+	unsigned int index;
+	unsigned int length = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[length] != '\0')
+		length++;
+
+	new = malloc(length * sizeof(char));
+
+	if (new == NULL)
+		return (NULL);
+
+	for (index = 0; index < length; index++)
+		new[index] = str[index];
+
+	return (new);
+}
