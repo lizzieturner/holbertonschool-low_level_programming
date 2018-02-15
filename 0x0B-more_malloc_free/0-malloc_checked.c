e3e6e5fcc7c52a595cd2b73d4ae84c298b8d5a0c
lizzieturner <hello@lizzieturner.com>
2018-02-15 20:03:41 +0000

    Create 0-malloc_checked.c

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - allocate memory using malloc
+ * @b: size of memory to be allocated
+ *
+ * Return: 98 if malloc fails, pointer to memory allocated otherwise
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *new_mem;
+
+	if (b > INT_MAX)
+		b = (double)b;
+
+	new_mem = malloc(b);
+
+	if (new_mem == NULL)
+		exit (98);
+
+	else
+		return (new_mem);
+}
