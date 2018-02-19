b1209c0d09ef875fada6f64c89e86ecc9524a89a
lizzieturner <hello@lizzieturner.com>
2018-02-19 21:54:23 +0000

    Fix betty errors in dog.h

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,28 +1,26 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
-int _putchar(char c);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
 /**
- * struct dog - dog
+ * struct dog - dog information
  * @name: name of the dog
- * @age: age of the dog
+ * @age:  age of the dog
  * @owner: name of the dog's owner
  *
  * Description: struct dog provides information about individual dogs,
  * including name of the dog, age of the dog, and name of the dog's owner
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 }
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
