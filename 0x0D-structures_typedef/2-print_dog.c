d43685a06334d53305e1c528515db60f8f21ba2e
lizzieturner <hello@lizzieturner.com>
2018-02-19 23:21:08 +0000

    Add edge cases to 2-print_dog.c

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -22,7 +22,7 @@ void print_dog(struct dog *d)
 		if (d->age != 0.0)
 			printf("Age: %f\n", d->age);
 		else
-			printf("(nil)\n");
+			printf("Age: (nil)\n");
 
 		printf("Owner: %s\n", d->owner);
 	}
