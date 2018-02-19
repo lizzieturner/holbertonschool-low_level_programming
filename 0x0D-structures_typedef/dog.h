c49a719f9ac4ccaae72fb083642f29e3d69d1ab2
lizzieturner <hello@lizzieturner.com>
2018-02-19 21:50:07 +0000

    Add struct dog docu in dog.h

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -7,7 +7,18 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-struct dog {
+/**
+ * struct dog - dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the dog's owner
+ *
+ * Description: struct dog provides information about individual dogs,
+ * including name of the dog, age of the dog, and name of the dog's owner
+ */
+
+typedef struct dog
+{
 	char *name;
 	float age;
 	char *owner;
