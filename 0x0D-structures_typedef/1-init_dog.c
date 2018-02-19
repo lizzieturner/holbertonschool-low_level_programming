77c53e87977082c684d60ee3fa908a8f24b0d9fa
lizzieturner <hello@lizzieturner.com>
2018-02-19 23:11:30 +0000

    Create 1-init_dog.c

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - creates a new dog
+ * @d: structure containing all dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of dog's owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
+}
