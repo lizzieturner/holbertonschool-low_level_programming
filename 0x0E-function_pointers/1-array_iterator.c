6438cc6f97acb034075b5af679e3cff83c978d53
lizzieturner <hello@lizzieturner.com>
2018-02-21 21:48:58 +0000

    Fix betty errors in 1-array_iterator.c

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -1,9 +1,12 @@
-#include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "function_pointers.h"
 
 /**
  * array_iterator - executes a function on each element of an array
+ * @array: array
+ * @size: size of array
+ * @action: function to execute
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
