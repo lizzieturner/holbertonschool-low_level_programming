fa0b0f9378fe7d3c3e9f5df9cb1b6922df3b5035
lizzieturner <hello@lizzieturner.com>
2018-02-21 21:47:03 +0000

    Fix betty errors in 0-print_name.c

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -1,9 +1,11 @@
-#include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "function_pointers.h"
 
 /**
  * print_name - prints a name
+ * @name: name to print
+ * @f: function pointer pointing to print function
  */
 
 void print_name(char *name, void (*f)(char *))
