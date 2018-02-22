628d68da0b67d23b155e590cc7a55709bbd8a6f3
lizzieturner <hello@lizzieturner.com>
2018-02-22 00:38:14 +0000

    Change null case in 3-get_op_func.c

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -21,9 +21,9 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL && s[1] == '\0')
+	while (ops[i].op != NULL)
 	{
-		if (*s == *ops[i].op)
+		if (*s == *ops[i].op && s[1] == '\0')
 			return (ops[i].f);
 		i++;
 	}
