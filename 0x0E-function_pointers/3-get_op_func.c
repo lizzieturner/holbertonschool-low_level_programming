e79056122a27f818ef519d0d39a4ce50b28ca0b7
lizzieturner <hello@lizzieturner.com>
2018-02-22 01:01:26 +0000

    Change conditional to .f in 3-get_op_func.c

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -3,7 +3,7 @@
 #include "3-calc.h"
 
 /**
- * get_op_func - selects the correct function to perform on arguments
+ * *get_op_func - selects the correct function to perform on arguments
  * @s: operator passed to arguments
  *
  * Return: pointer to correct function
@@ -21,7 +21,7 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL)
+	while (ops[i].f != NULL)
 	{
 		if (*s == *(ops[i].op) && s[1] == '\0')
 			return (ops[i].f);
