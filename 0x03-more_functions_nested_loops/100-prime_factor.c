7bb1eda3e2993324b7b271d817361b6ffa3b7dd7
lizzieturner <hello@lizzieturner.com>
2018-02-23 06:49:02 +0000

    Create 100-prime_factor.c

---
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	unsigned long test, num;
+
+	num = 612852475143;
+
+	for (test = 3; test < num; test ++)
+	{
+		if (num % test == 0)
+		    num /= test;
+	}
+
+	printf("%lu\n", num);
+}
