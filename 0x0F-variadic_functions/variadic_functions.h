4ddfe3f73dd7e6390bc78d28139c7675472c6bdb
lizzieturner <hello@lizzieturner.com>
2018-02-24 00:00:39 +0000

    Add prototypes from 3-print_all.c to variadic_functions.h

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,5 +1,28 @@
+#ifndef _VARIADIC_FUNCTIONS_H
+#define _VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list list);
+void print_int(va_list list);
+void print_float(va_list list);
+void print_string(va_list list);
+
+/**
+ * struct what_type - struct what_type
+ *
+ * @c: the type identifier
+ * @f: print function for each type
+ */
+
+typedef struct what_type
+{
+	char *c;
+	void (*f)(va_list list);
+} type;
+
+#endif
