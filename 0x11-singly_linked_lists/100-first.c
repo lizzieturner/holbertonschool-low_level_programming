0bb70cb6a97f6817a64020a02d6792aa3446d760
Lizzie Turner <hello@lizzieturner.com>
2018-03-02 20:02:26 +0000

    Create 100-first.c

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * first - prints text before main function executes
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
