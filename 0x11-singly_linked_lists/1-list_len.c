f060fc5cbea71ef8eacaf3f071bacfb580550eb5
Lizzie Turner <hello@lizzieturner.com>
2018-03-02 19:10:31 +0000

    Fix betty errors in 1-list_len.c

--- 0x11-singly_linked_lists/1-list_len.c
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -11,11 +11,14 @@ size_t list_len(const list_t *h)
 {
 	size_t count = 1;
 
+	if (h == NULL)
+		return (0);
+
 	while (h->next != NULL)
 	{
 		count++;
 		h = h->next;
 	}
 
-	return(count);
+	return (count);
 }
