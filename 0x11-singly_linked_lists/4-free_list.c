e912cfc1f112458fac4cfbdcce35e73c53e618e3
Lizzie Turner <hello@lizzieturner.com>
2018-03-02 19:56:01 +0000

    Create 4-free_list.c

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: list to free
+ */
+
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		free(head->str);
+		head = head->next;
+	}
+
+	free(head);
+
+}
