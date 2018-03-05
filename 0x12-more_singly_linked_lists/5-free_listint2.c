35107ed6aebd73f4f0738b03a98a94021ef884e7
Lizzie Turner <hello@lizzieturner.com>
2018-03-05 22:20:39 +0000

    Create 5-free_listint2.c

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list and sets head to NULL
+ * @head: list of type listint_t
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	if (*head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		current = *head;
+		free(head);
+		*head = current->next;
+	}
+}
