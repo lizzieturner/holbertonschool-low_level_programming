3dec647e4efdb37e6ea66ba8a9880a2d28d1a027
Lizzie Turner <hello@lizzieturner.com>
2018-03-06 22:28:53 +0000

    Find start of loop in 103-find_loop.c:

--- 0x12-more_singly_linked_lists/103-find_loop.c
+++ 0x12-more_singly_linked_lists/103-find_loop.c
@@ -11,14 +11,30 @@ listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *slow = head;
 	listint_t *fast = head;
+	int loop_exists = 0;
 
-	while (fast != NULL)
+	while (slow != NULL && fast != NULL && fast->next != NULL)
 	{
 		slow = slow->next;
 		fast = (fast->next)->next;
 
 		if (slow == fast)
-			return (fast);
+		{
+			loop_exists = 1;
+			break;
+		}
+	}
+
+	if (loop_exists == 1)
+	{
+		slow = head;
+		while (slow != fast)
+		{
+			slow = slow->next;
+			fast = fast->next;
+		}
+
+		return (slow);
 	}
 
 	return (NULL);
