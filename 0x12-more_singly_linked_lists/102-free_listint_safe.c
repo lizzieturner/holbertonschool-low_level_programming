0957e8fda092fa72bc0cc284155bed5f013fa311
Lizzie Turner <hello@lizzieturner.com>
2018-03-06 21:31:57 +0000

    Create 102-free_listint_safe.c

---
+++ 0x12-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a list (safe version)
+ * @h: linked list of type listint_t
+ *
+ * Return: number of nodes in freed list
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *current, *runner;
+	size_t h_i, r_i;
+
+	if (h == NULL)
+		return (0);
+
+	current = *h;
+	h_i = 0;
+
+	while (*h != NULL)
+	{
+		runner = *h;
+		for (r_i = 0; r_i < h_i; r_i++)
+		{
+			if (runner == current)
+			{
+				return (h_i);
+			}
+
+			runner = runner->next;
+		}
+		current = (*h)->next;
+		free(*h);
+		*h = current;
+		h_i++;
+	}
+
+	free(h);
+	return (h_i);
+}
