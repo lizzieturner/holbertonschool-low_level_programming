031803ac1fae9c53a37921173017d345676f3a8f
Lizzie Turner <hello@lizzieturner.com>
2018-03-09 00:24:21 +0000

    Create 2-get_bit.c

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * get_bit - returns value of a bit at a given index
+ * @n: binary number
+ * @index: index to find bit at
+ *
+ * Return: value of bit at index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > n)
+		return (-1);
+
+	return (n >> index & 1);
+}
