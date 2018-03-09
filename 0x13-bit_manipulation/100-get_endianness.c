11532d061b61a80d75db59a0fe89fd5f07576881
Lizzie Turner <hello@lizzieturner.com>
2018-03-09 01:37:21 +0000

    Create 100-get_endianness.c

---
+++ 0x13-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * get_endianness - checks endianness of a system
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	int i = 1;
+	char *c = (char *)&i;
+
+	return (int)(*c);
+}
