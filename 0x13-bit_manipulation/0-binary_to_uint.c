79d351c522e282435028814e974dd3e68a51dd05
Lizzie Turner <hello@lizzieturner.com>
2018-03-09 00:07:39 +0000

    Fix betty error in 0-binary_to_uint.c

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 	for (index = 0; b[index] != '\0'; index++)
 	{
 		if (b[index] != '0' && b[index] != '1')
-			return(0);
+			return (0);
 
 		num <<= 1;
 
