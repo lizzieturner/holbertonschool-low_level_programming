a9c3fa62020bc59b5416cf60686ccf55b4dd4c50
Lizzie Turner <hello@lizzieturner.com>
2018-03-13 22:24:02 +0000

    Add 'text_content == NULL ' check to 2-append_text_to_file.c

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -31,13 +31,15 @@ int append_text_to_file(const char *filename, char *text_content)
 	size_t length;
 	ssize_t write_actual;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_APPEND | O_RDWR);
-
 	if (fd == -1)
 		return (-1);
+
+	if (text_content == NULL)
+		return (1);
 
 	length = _strlen(text_content);
 
