feb76768894950289fce576b37af4c762cf50082
Lizzie Turner <hello@lizzieturner.com>
2018-03-13 21:39:36 +0000

    Add close_check to 2-append_text_to_file.c

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -46,7 +46,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (write_actual == -1)
 		return (-1);
 
-	close(fd);
+	close_check = close(fd);
 
 	if (close_check == -1)
 		return (-1);
