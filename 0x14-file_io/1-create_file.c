1856c3e5ce9d363d2c1d1bacceec792a92bc67dc
Lizzie Turner <hello@lizzieturner.com>
2018-03-13 21:53:12 +0000

    Add 'text_content ==  NULL' test to 1-create_file.c

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -31,8 +31,11 @@ int create_file(const char *filename, char *text_content)
 	size_t length;
 	ssize_t write_actual;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
+
+	if (text_content == NULL)
+		return (1);
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
 	if (fd == -1)
