774be322ff2b4f44b109673231d88a744c21e6f9
Lizzie Turner <hello@lizzieturner.com>
2018-04-17 06:17:28 +0000

    Add -q flag to wget

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O tmp/unrandom.so https://github.com/lizzieturner/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/unrandom.so 
+wget -q -O tmp/unrandom.so https://github.com/lizzieturner/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/unrandom.so 
 export LD_PRELOAD=tmp/unrandom.so
