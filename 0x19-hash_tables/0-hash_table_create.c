f7f6681c7c884931525bf8f6cbf633050d1f00ce
Lizzie Turner <hello@lizzieturner.com>
2018-05-11 21:18:08 +0000

    Add second malloc

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -11,9 +11,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new;
 
-	new = malloc(sizeof(hash_table_t) * size);
+	new = malloc(sizeof(hash_table_t));
 	if (new == NULL)
 		return (NULL);
-
+	new->size = size;
+	new->array = malloc(sizeof(hash_table_t) * new->size);
 	return (new);
 }
