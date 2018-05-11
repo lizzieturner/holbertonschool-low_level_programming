3ad90445d9025064eaf67587c2c8b6bd258dd144
Lizzie Turner <hello@lizzieturner.com>
2018-05-11 21:30:11 +0000

    Initialize array to NULL

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -10,11 +10,20 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new;
+	int i;
 
 	new = malloc(sizeof(hash_table_t));
 	if (new == NULL)
 		return (NULL);
 	new->size = size;
 	new->array = calloc(new->size, sizeof(hash_node_t));
+	if (new->array == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		new->array[i] = NULL;
+
 	return (new);
 }
