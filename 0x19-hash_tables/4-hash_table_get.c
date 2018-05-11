9c421f1c5e288a496b4fbda2b2e67e5fc8d81967
Lizzie Turner <hello@lizzieturner.com>
2018-05-11 22:21:22 +0000

    Create 4-hash_table_get.c

---
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - returns value associated with a certain key
+ * @ht: hash table
+ * @key: key
+ *
+ * Return: value
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	char *value;
+	unsigned long int index;
+	hash_node_t *current;
+
+	index = key_index((unsigned char *)key, ht->size);
+	current = ht->array[index];
+	if (current == NULL)
+		return (NULL);
+	value = current->value;
+	return (value);
+}
