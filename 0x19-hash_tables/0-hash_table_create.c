484a56b87f71d39f51adf118ab0334c75b44d65e
Lizzie Turner <hello@lizzieturner.com>
2018-05-11 21:21:54 +0000

    Change second malloc to calloc

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -15,6 +15,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new == NULL)
 		return (NULL);
 	new->size = size;
-	new->array = malloc(sizeof(hash_table_t) * new->size);
+	new->array = calloc(new->size, sizeof(hash_node_t));
 	return (new);
 }
