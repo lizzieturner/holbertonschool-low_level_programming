715b2b1109b9c4adfd7d58196697d8b1295b58bf
Lizzie Turner <hello@lizzieturner.com>
2018-05-11 21:37:32 +0000

    Update malloc value

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new_ht == NULL)
 		return (NULL);
 	new_ht->size = size;
-	new_ht->array = malloc(sizeof(hash_table_t) * size);
+	new_ht->array = malloc(sizeof(hash_table_t *) * size);
 	if (new_ht->array == NULL)
 	{
 		free(new_ht);
