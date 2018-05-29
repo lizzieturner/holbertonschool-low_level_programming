d2909ea4b9edebaa45308d4167e65478b875d90f
Lizzie Turner <hello@lizzieturner.com>
2018-05-29 23:05:47 +0000

    Create 100-shell_sort

---
+++ 0x1A-sorting_algorithms/100-shell_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts integer array using shell sort algorithm
+ * @array: array to sort
+ * @size: size of array
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	size_t i, j, gap = 1;
+	int tmp;
+
+	if (array == NULL || size < 1)
+		return;
+	while (gap < (size - 1) / 3)
+		gap = (gap * 3) + 1;
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			tmp = array[i];
+			for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+			array[j] = tmp;
+		}
+		gap = (gap - 1) / 3;
+		print_array(array, size);
+	}
+}
