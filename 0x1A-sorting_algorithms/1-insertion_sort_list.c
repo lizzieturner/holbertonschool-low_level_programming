0b918ec9a27d1503b8835a164dbb629024066669
Lizzie Turner <hello@lizzieturner.com>
2018-05-29 18:05:19 +0000

    Create 1-insertion_sort_list

---
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sort using the insertion sort algorithm
+ * @list: doubly linked list to sort
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *runner;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	for (runner = (*list)->next; runner != NULL; runner = runner->next)
+	{
+		while (runner->prev != NULL && runner->n < runner->prev->n)
+		{
+			runner->prev->next = runner->next;
+			if (runner->next != NULL)
+				runner->next->prev = runner->prev;
+			runner->next = runner->prev;
+			runner->prev = runner->prev->prev;
+			runner->next->prev = runner;
+			if (runner->prev == NULL)
+				*list = runner;
+			else
+				runner->prev->next = runner;
+			print_list(*list);
+		}
+	}
+}
